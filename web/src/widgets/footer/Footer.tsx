ff1787a7430ed7415d2d598470e4d9c5754f6e9c
Pankaj Gusain <gusainpankaj1999@gmail.com>
2026-06-03 21:12:09 +0530

    github action commit

--- web/src/widgets/footer/Footer.tsx
+++ web/src/widgets/footer/Footer.tsx
@@ -5,6 +5,7 @@ export const Footer = () => {
   console.log("Footer styles", styles);
   console.log("Footer styles.footer", styles.footer);
   console.log("Footer className", styles.footer);
+  coonsole.log("Footer className type", typeof styles.footer);
 
   return (
     <footer className={styles.footer}>
