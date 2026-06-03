6354b549ecf0e9defb371019b48a333459c9d2fd
Pankaj Gusain <gusainpankaj1999@gmail.com>
2026-06-03 21:13:14 +0530

    check

--- web/src/widgets/footer/Footer.tsx
+++ web/src/widgets/footer/Footer.tsx
@@ -4,8 +4,6 @@ export const Footer = () => {
   console.log("Footer rendered");
   console.log("Footer styles", styles);
   console.log("Footer styles.footer", styles.footer);
-  console.log("Footer className", styles.footer);
-  coonsole.log("Footer className type", typeof styles.footer);
 
   return (
     <footer className={styles.footer}>
