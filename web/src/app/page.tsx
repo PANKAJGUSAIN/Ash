7a3e4b12091186a60eb6bcf6fef149cb70b4dbb0
Pankaj Gusain <pankaj.gusain@sprinklr.com>
2026-06-03 19:34:44 +0530

    add web setup

---
+++ web/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/widgets/footer";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1>Ash</h1>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
