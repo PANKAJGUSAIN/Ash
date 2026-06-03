dbe9cb490b470ac31f2a2d56a4abc5a6b935ceff
Pankaj Gusain <gusainpankaj1999@gmail.com>
2026-06-03 20:41:06 +0530

    test github hooks

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -3,11 +3,18 @@ import { defineConfig, globalIgnores } from "eslint/config";
 export default defineConfig([
   {
     extends: ["next/core-web-vitals", "next/typescript"],
+
     rules: {
-      // spacing between imports and code
       "padding-line-between-statements": [
         "error",
+
+        // space after imports
         { blankLine: "always", prev: "import", next: "*" },
+
+        // space before exports
+        { blankLine: "always", prev: "*", next: "export" },
+
+        // space before return (optional but useful)
         { blankLine: "always", prev: "*", next: "return" },
       ],
     },
--- web/src/widgets/footer/Footer.tsx
+++ web/src/widgets/footer/Footer.tsx
@@ -1,5 +1,6 @@
 import styles from "./footer.module.scss";
 export const Footer = () => {
+  console.log("Footer rendered");
   return (
     <footer className={styles.footer}>
       <div>ASH</div>
